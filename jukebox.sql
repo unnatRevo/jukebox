d382136612e141b50f07bcf415d5279b35a09f95
Unnat Pandya <upandya@thebeastapps.com>
2016-10-11 18:44:56 +0530

    Signup designed.

--- jukebox.sql
+++ jukebox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2016 at 11:52 AM
+-- Generation Time: Oct 11, 2016 at 01:14 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -27,12 +27,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `filedetails` (
-  `username` varchar(30) NOT NULL,
-  `filename` varchar(500) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `filename` varchar(250) NOT NULL,
   `filesize` decimal(5,2) NOT NULL,
+  `filetype` varchar(50) NOT NULL,
+  `fileextension` varchar(10) NOT NULL,
   `filepath` varchar(500) NOT NULL,
-  `filetype` varchar(5) NOT NULL
+  `fileuploaderror` int(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `filedetails`
+--
+
+INSERT INTO `filedetails` (`ID`, `username`, `filename`, `filesize`, `filetype`, `fileextension`, `filepath`, `fileuploaderror`) VALUES
+(1, 'demo', 'temp', '5.20', 'JPG', 'aa', 'aa', 1),
+(2, 'demo', 'temp', '5.20', 'JPG', 'aa', 'aa', 1),
+(3, 'demo', 'I Miss U.mp3', '0.00', '', 'mp3', '../../userdata/demoI Miss U.mp3', 1),
+(4, 'demo', 'Haar Gya Dil.mp3', '6.21', 'audio/mpeg', 'mp3', '../../userdata/demo/Haar Gya Dil.mp3', 0),
+(5, 'demo', 'I Miss U.mp3', '4.30', 'audio/mpeg', 'mp3', '../../userdata/demo/I Miss U.mp3', 0),
+(6, 'test', 'Aisi Diwangi ko.mp3', '4.57', 'audio/mpeg', 'mp3', '../../userdata/test/Aisi Diwangi ko.mp3', 0);
 
 -- --------------------------------------------------------
 
@@ -73,6 +88,12 @@ CREATE TABLE `userdetails` (
 --
 
 --
+-- Indexes for table `filedetails`
+--
+ALTER TABLE `filedetails`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `logindetail`
 --
 ALTER TABLE `logindetail`
@@ -88,6 +109,11 @@ ALTER TABLE `userdetails`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `filedetails`
+--
+ALTER TABLE `filedetails`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `logindetail`
 --
